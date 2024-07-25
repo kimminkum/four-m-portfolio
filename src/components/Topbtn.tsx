30ebdeed61e9e6f9181047533ee42200a9829211
kimminkum <h24breaker@naver.com>
2024-07-25 17:34:45 +0900

    components 3

--- src/components/Topbtn.tsx
+++ src/components/Topbtn.tsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const TopBtn = styled.div`
-  position: sticky;
-  bottom: 0; // 하단에 고정
-  right: 5%; // 우측에 고정
-  width: 50px;
-  height: 50px;
-
-  background-color: #aaa;
+// bottom 값을 동적으로 조정하기 위해 props를 추가합니다.
+const TopBtn = styled.div<{ bottom: number }>`
+  position: fixed;
+  bottom: ${(props) => props.bottom}px; // 동적으로 bottom 값을 조정합니다.
+  right: 5%;
+  max-width: 50px;
+  max-height: 50px;
+  width: 8vw;
+  height: 8vw;
+  background-color: #ddd;
+  z-index: 8;
+  border-radius: 50%;
 `;
 
 const Topbtn: React.FC = () => {
-  return (
-    <>
-      <TopBtn></TopBtn>
-    </>
-  );
+  // isVisible 대신 bottom 값을 상태로 관리합니다.
+  const [bottom, setBottom] = useState(30); // 기본값은 30px
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const footerHeight = 800; // 푸터의 높이
+      const scrollHeight = document.body.scrollHeight;
+      const viewportHeight = window.innerHeight;
+      const currentScrollPosition = window.scrollY;
+      const footerStart = scrollHeight - footerHeight;
+
+      // 스크롤 위치가 푸터 시작 위치보다 작으면, 푸터 바로 위에 위치하도록 bottom 값을 조정합니다.
+      if (currentScrollPosition + viewportHeight < footerStart) {
+        setBottom(30); // 기본 위치
+      } else {
+        // 푸터 바로 위에 고정시키기 위해 viewportHeight에서 푸터 시작 위치까지의 거리를 계산합니다.
+        const dynamicBottom =
+          currentScrollPosition + viewportHeight - footerStart + 30; // 30은 기본 여백
+        setBottom(dynamicBottom);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return <>{<TopBtn bottom={bottom}></TopBtn>}</>;
 };
 
 export default Topbtn;
