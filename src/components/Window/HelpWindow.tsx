009156505541d84e2f037734008e2008b00cf59b
kimminkum <h24breaker@naver.com>
2025-03-06 10:08:34 +0900

    Window Folder

---
+++ src/components/Window/HelpWindow.tsx
@@ -0,0 +1,9 @@
+import styled, { keyframes } from "styled-components";
+import React from "react";
+
+// 사용 예시
+function HelpWindow() {
+  return <></>;
+}
+
+export default HelpWindow;
---
+++ src/components/Window/UiWindow.tsx
@@ -0,0 +1,9 @@
+import styled, { keyframes } from "styled-components";
+import React from "react";
+
+// 사용 예시
+function UiWindow() {
+  return <></>;
+}
+
+export default UiWindow;
